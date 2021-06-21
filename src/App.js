dad821c7b1549820b545326a90be8c6300bd50eb
shahjalal <shahjalalkhan215@gmail.com>
2021-06-21 18:12:34 +0600

    sceound Commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,58 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-import {Cards, Carts, CuntryPicker} from './Components/Index'
+import { Form } from 'react-bootstrap'
+import { Cards, Carts, CuntryPicker } from './Components/Index'
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [Data, setData] = useState([])
+
+  const featchData = async () => {
+    try {
+      const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get('https://covid19.mathdro.id/api')
+      const response = { confirmed, recovered, deaths, lastUpdate }
+      setData(response)
+    }
+    catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    featchData()
+  }, [])
+
+
   return (
     <div className="App">
-      <Cards />
+      <div className="container">
+        <div className="title">
+          <div className="row">
+            <div className="col-md-6">
+              <h1 className="text-left">Covid Tracker App</h1>
+            </div>
+            <div className="col-md-6">
+
+            </div>
+          </div>
+        </div>
+        <div className="covid-card">
+          <div className="row">
+            <div className="col-md-8">
+              <Cards Data={Data} />
+            </div>
+            <div className="col-md-4">
+              
+            </div>
+          </div>
+        </div>
+        
+        
+      </div>
       <Carts />
-      <CuntryPicker />
+      
     </div>
   );
 }
--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -1,10 +1,46 @@
 import React from 'react'
+import CountUp from 'react-countup';
+import './Cards.css'
 
-const Cards = () => {
+const Cards = ({ Data: { confirmed, recovered, deaths, lastUpdate } }) => {
+    if (!confirmed) {
+        return "Looding..."
+    }
     return (
-        <div>
-            ssssssss
-        </div>
+        <>
+            <section className="headers">
+                <div className="card infected">
+                    <div className="card-body">
+                        <h3 className="text-secondary">Infected</h3>
+                        <h4>
+                            <CountUp start={0} duration={2.5} separator="," end={confirmed.value} />
+                        </h4>
+                        <h5 className="text-secondary">({new Date(lastUpdate).toDateString()})</h5>
+                        <h6>Number of active cases of COVID-19</h6>
+                    </div>
+                </div>
+
+                <div className="card recovered">
+                    <div className="card-body">
+                        <h3 className="text-secondary">Recovered</h3>
+                        <h4> <CountUp start={0} duration={2.5} separator="," end={recovered.value} /></h4>
+                        <h5 className="text-secondary">({new Date(lastUpdate).toDateString()})</h5>
+                        <h6>Number of active recoveris from COVID-19</h6>
+                    </div>
+                </div>
+
+                <div className="card death">
+                    <div className="card-body">
+                        <h3 className="text-secondary">Deaths</h3>
+                        <h4><CountUp start={0} duration={2.5} separator="," end={deaths.value} /> </h4>
+                        <h5 className="text-secondary">({new Date(lastUpdate).toDateString()})</h5>
+                        <h6>Number of deaths caused by COVID-19</h6>
+                    </div>
+                </div>
+
+
+            </section>
+        </>
     )
 }
 
